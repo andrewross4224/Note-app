5c35d352e255393a3134a4f04738afbf8c32a5ba
Andrew Ross <andrew.ross4224@gmail.com>
2023-11-05 17:59:30 -0500

    made basic server.js file

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const path = require('path')
+const { env } = require('process')
+
+const PORT = process.env.PORT || 3001;
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({extended: true}));
+app.use(express.static('public'));
+
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/public/index.html')))
+
+app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, 'public/notes.html')))
+
+app.listen(PORT, () => console.log(`App listening on port ${PORT}`))
